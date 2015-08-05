bb38fb76f529a88d2e4616a6470a6544c086a878
Martin <martinlcalvert@gmail.com>
2015-08-04 23:04:56 -0700

    Add full calculator functionality

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
 
     // Variables
     var userIsTypingNumber = false
+    var opperandStack = Array<Double>()
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
+    var lastOperation = UIButton()
     
     // Methods
     @IBAction func appendDigit(sender: UIButton) {
@@ -26,11 +36,49 @@ class ViewController: UIViewController {
             display.text = digit
             userIsTypingNumber = true
         }
-        
     }
     
-    @IBAction func clear() {
-        display.text = "0"
+    @IBAction func enter() {
+        userIsTypingNumber = false
+        opperandStack.append(displayValue)
+        if opperandStack.count >= 2{
+            operate(lastOperation)
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        lastOperation = sender
+        if userIsTypingNumber{
+            enter()
+        }
+        switch operation{
+            case "✖️": performOperation { $0 * $1 }
+            case "➗": performOperation { $0 / $1 }
+            case "➖": performOperation { $0 - $1 }
+            case "➕": performOperation { $0 + $1 }
+            case "√": performOperation { sqrt($0) }
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if opperandStack.count >= 2{
+            displayValue = operation(opperandStack.removeLast(), opperandStack.removeLast())
+            opperandStack.append(displayValue)
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if opperandStack.count >= 1{
+            displayValue = operation(opperandStack.removeLast())
+            opperandStack.append(displayValue)
+        }
+    }
+    
+    @IBAction func reset(sender: UIButton) {
+        displayValue = 0
+        opperandStack = Array<Double>()
         userIsTypingNumber = false
     }
 
