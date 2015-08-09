a24d56f36e509d388e2373a8fe28755ed368148d
Martin <martinlcalvert@gmail.com>
2015-08-09 02:41:44 -0700

    Finish project 1 with my own enter style

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     // Outlets
     @IBOutlet weak var display: UILabel!
 
+    @IBOutlet weak var history: UILabel!
     // Variables
     var userIsTypingNumber = false
     var opperandStack = Array<Double>()
@@ -24,11 +25,18 @@ class ViewController: UIViewController {
             display.text = "\(newValue)"
         }
     }
-    var lastOperation = UIButton()
+    var lastOperation = UIButton?()
     
     // Methods
     @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
+        var digit = sender.currentTitle!
+        switch digit{
+            case "pi": digit = "\(M_PI)"
+            case ".":
+                if display.text!.rangeOfString(".") != nil {
+                    digit = "" }
+            default: break
+        }
         if userIsTypingNumber{
             display.text = display.text! + digit
         }
@@ -36,13 +44,14 @@ class ViewController: UIViewController {
             display.text = digit
             userIsTypingNumber = true
         }
+        history.text = history.text! + " \(digit)"
     }
     
     @IBAction func enter() {
         userIsTypingNumber = false
         opperandStack.append(displayValue)
-        if opperandStack.count >= 2{
-            operate(lastOperation)
+        if lastOperation != nil{
+            operate(lastOperation!)
         }
     }
     
@@ -50,7 +59,8 @@ class ViewController: UIViewController {
         let operation = sender.currentTitle!
         lastOperation = sender
         if userIsTypingNumber{
-            enter()
+            userIsTypingNumber = false
+            opperandStack.append(displayValue)
         }
         switch operation{
             case "✖️": performOperation { $0 * $1 }
@@ -58,7 +68,12 @@ class ViewController: UIViewController {
             case "➖": performOperation { $0 - $1 }
             case "➕": performOperation { $0 + $1 }
             case "√": performOperation { sqrt($0) }
+            case "sin": performOperation { sin($0) }
+            case "cos": performOperation { cos($0) }
             default: break
+        }
+        if lastOperation != nil{
+            history.text = history.text! + " \(lastOperation!.currentTitle!)"
         }
     }
     
@@ -66,6 +81,7 @@ class ViewController: UIViewController {
         if opperandStack.count >= 2{
             displayValue = operation(opperandStack.removeLast(), opperandStack.removeLast())
             opperandStack.append(displayValue)
+            lastOperation = nil
         }
     }
     
@@ -73,6 +89,8 @@ class ViewController: UIViewController {
         if opperandStack.count >= 1{
             displayValue = operation(opperandStack.removeLast())
             opperandStack.append(displayValue)
+            history.text = history.text! + " \(lastOperation!.currentTitle!)"
+            lastOperation = nil
         }
     }
     
@@ -80,6 +98,7 @@ class ViewController: UIViewController {
         displayValue = 0
         opperandStack = Array<Double>()
         userIsTypingNumber = false
+        history.text = ""
     }
 
 }
